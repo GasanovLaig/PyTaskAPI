01fbb1319206d9cf16f957251eea4b6ab6d13ebd
GasanovLaig <59813066+GasanovLaig@users.noreply.github.com>
2026-06-25 23:58:08 +0700

    feat: внедрить систему асинхронных миграций базы данных Alembic
    - Инициализировать Alembic с использованием асинхронного шаблона async
    - Настроить подключение к базе данных в файле alembic.ini
    - Подключить метаданные Base и импортировать все ORM-модели в migrations/env.py
    - Сгенерировать и применить первую автоматическую миграцию инициализации таблиц
    - Удалить вызов Base.metadata.create_all из lifespan-события в main.py

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from app.core.database import async_engine, Base
 from app.api.v1.auth import router as auth_router
 from app.api.v1.projects import router as project_router
 from app.api.v1.tasks import router as task_router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with async_engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-app = FastAPI(title="PyTaskAPI", lifespan=lifespan)
+app = FastAPI(title="PyTaskAPI")
 
 app.include_router(auth_router)
 app.include_router(project_router)
 app.include_router(task_router)
-
-@app.get("/")
-def main():
-    return {"message": "FastAPI successfully launched!"}
