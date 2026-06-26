3248f8a93c5f1f310efa962e352a3b2ce00803a1
GasanovLaig <59813066+GasanovLaig@users.noreply.github.com>
2026-06-26 15:47:09 +0700

    feat: ограничить доступ к созданию проектов для неавторизованных пользователей
    - Реализовать функцию-зависимость get_current_user для валидации JWT-токенов
    - Защитить эндпоинт POST /projects с помощью проверки текущей сессии пользователя
    - Исключить явную передачу user_id из параметров запроса

--- app/api/v1/projects.py
+++ app/api/v1/projects.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
+from app.core.security import get_current_user
 from app.models.project import Project
+from app.models.user import User
 from app.repositories.project import ProjectRepository
 from app.repositories.user import UserRepository
 from app.schemas.project import ProjectCreate, ProjectResponse
@@ -11,12 +13,16 @@ from app.services.project import ProjectService
 router = APIRouter(tags=["Проекты"])
 
 @router.post("/projects", response_model=ProjectResponse)
-async def create_project(project_data: ProjectCreate, user_id:int, db: AsyncSession = Depends(get_db)) -> Project:
+async def create_project(
+    project_data: ProjectCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+) -> Project:
     user_repository = UserRepository(session=db)
     project_repository = ProjectRepository(session=db)
     project_service = ProjectService(project_repo=project_repository, user_repo=user_repository)
     
-    return await project_service.create_new_project(user_id=user_id, project_data=project_data)
+    return await project_service.create_new_project(user_id=current_user.id, project_data=project_data)
 
 @router.get("/users/{user_id}/projects", response_model=list[ProjectResponse])
 async def get_projects_by_user(user_id: int, db: AsyncSession = Depends(get_db)) -> list[Project]:
--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,14 @@
 from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 import jwt
 from pwdlib import PasswordHash
 from pwdlib.hashers.argon2 import Argon2Hasher
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import get_db
+from app.models.user import User
+from app.repositories.user import UserRepository
 
 password_hash = PasswordHash([Argon2Hasher()])
 
@@ -23,3 +30,33 @@ def create_access_token(data: dict) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось валидировать учетные данные",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        
+    except jwt.PyJWTError:
+        raise credentials_exception
+    
+    user_repository = UserRepository(session=db)
+    user = await user_repository.get_by_email(email)
+    
+    if user is None:
+        raise credentials_exception
+    
+    return user
+    
