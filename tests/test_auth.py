535cd687ac3036a4c3d4157d9f7ef3c96e451dfb
GasanovLaig <59813066+GasanovLaig@users.noreply.github.com>
2026-06-27 17:01:42 +0700

    test: добавить проверку уникальности email при регистрации пользователя
    - Покрыть тестами сценарий отправки дублирующих регистрационных данных
    - Верифицировать возврат HTTP-статуса 400 при конфликте уникальности
    - Убедиться в корректности текста возвращаемой ошибки

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,11 +7,28 @@ async def test_register_user_success(client: AsyncClient):
         "full_name": "User Tester"
     }
     
-    response = await client.post("auth/users", json=test_user_data)
+    response = await client.post("/auth/users", json=test_user_data)
     assert response.status_code == 200
+    
     response_data = response.json()
     assert response_data["email"] == test_user_data["email"]
     
     assert "password" not in response_data
     assert "hash_password" not in response_data
+    
+async def test_register_user_duplicate_email_fail(client: AsyncClient): 
+    test_user_data = {
+        "email": "duplicate_user@mail.ru",
+        "password": "super_secret_password_example!1@2#3$4%5^6&7*8(9)0",
+        "full_name": "User Tester"
+    }
+    
+    first_response = await client.post("/auth/users", json=test_user_data)
+    assert first_response.status_code == 200
+    
+    second_response = await client.post("/auth/users", json=test_user_data)
+    assert second_response.status_code == 400
+    
+    response_data = second_response.json()
+    assert response_data["detail"] == "Пользователь с таким email уже зарегистрирован"
     
