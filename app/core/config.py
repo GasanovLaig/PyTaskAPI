e3c140835405ec05babab233753dbaa8159e8662
GasanovLaig <59813066+GasanovLaig@users.noreply.github.com>
2026-07-25 22:55:54 +0700

    chore: настроить изолированную Docker-инфраструктуру для тестов
    - Развернуть выделенный сервис тестовой базы данных и кэша redis в compose.tests.yaml
    - Синхронизировать сетевые хосты контейнеров с параметрами тест-раннера
    - Выделить конфигурацию тестовой среды в обособленный файл переменных окружения
    - Добавить локальные файлы настроек тестирования в список исключений Git

--- app/core/config.py
+++ app/core/config.py
@@ -3,19 +3,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: int = 5432
     DB_NAME: str
     
-    AUTH_SECRET_KEY: str
+    AUTH_SECRET_KEY: str = "dd46ad1483074445be8bb4ad7b7eafb2a75be8bd418379457c8202b6f28a7241"
     AUTH_ALGORITHM: str = "HS256"
     AUTH_EXPIRATION: int = 30
     
-    REDIS_URL: str
+    REDIS_HOST: str = "127.0.0.1"
+    REDIS_PORT: int = 6379
     
-    MAIL_SMTP_HOST: str
-    MAIL_SMTP_PORT: int
-    MAIL_SENDER_EMAIL: str
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+    
+    MAIL_SMTP_HOST: str = "127.0.0.1"
+    MAIL_SMTP_PORT: int = 1025
+    MAIL_SENDER_EMAIL: str = "robot@pytaskapi.test"
     
     @property
     def DATABASE_URL_ASYNC(self) -> str:
@@ -24,7 +29,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        extra="ignore"
+        extra="ignore",
+        env_prefix=""
     )
     
 settings = Settings()
